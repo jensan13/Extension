cb2146c51dfd4c0a9ee43b651fd1e494f9ccb6b0
Jennifer Sanchez <jennifer.sanchez1@speeracademy.org>
2017-05-18 09:21:47 -0500

    v0.1.0.0 DBS EXT Upload

--- content.js
+++ content.js
@@ -1,13 +1,4 @@
-// content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-
-      // This line is new!
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-    }
-  }
-);
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placehold.it/350x150' + images[i].width + '/' + images[i].height;
+}
