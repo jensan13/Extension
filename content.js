e10b99d487c9e5ce87e76d85ada9bf98b89e7893
Jennifer Sanchez <jennifer.sanchez1@speeracademy.org>
2017-06-06 08:56:42 -0500

    v1.0.0.0 ~ Changed it to ask a prompt

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
-    $('div').hover(function(){
-        $(this).addClass('red');
-    });
-});
+var cakes = prompt("What's an evil number?");
+
+if (cakes == null || cakes== ""){
+    txt= "User didn't answer the question smh";
+} else {
+    txt = cakes + "is not an evil number";
+
+}
